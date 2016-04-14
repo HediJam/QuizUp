32a3b363167d7613b41eaf40a81f59d6eb8a74e0
pFathololumi <p16_f3@yahoo.com>
2016-04-14 22:47:04 +0430

    work on UI..

--- kooisup/src/ir/kooisup/jam/ListCountry.java
+++ kooisup/src/ir/kooisup/jam/ListCountry.java
@@ -1,6 +1,7 @@
 package ir.kooisup.jam;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import javax.faces.bean.ManagedBean;
@@ -9,9 +10,12 @@ import javax.faces.bean.ViewScoped;
 @ManagedBean
 @ViewScoped
 public class ListCountry{
+	
 	private Map<String,String> countries;
 	
 	public ListCountry(){
+		System.out.println("********");
+		countries = new HashMap<String, String>();
 		run();
 	}
 	
@@ -21,12 +25,13 @@ public class ListCountry{
 		
 		for (String countryCode : locales ) {
 			Locale obj = new Locale("", countryCode);
-			
+			System.out.println(obj.getDisplayCountry());
 			countries.put(obj.getCountry(), obj.getDisplayCountry());
 		}
 	}
 	
 	public Collection<String> getCountries() {
+		run();
 		System.out.println("--> "+countries.size());
         return countries.values();
     }
