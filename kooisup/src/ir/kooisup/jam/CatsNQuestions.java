f8572157172676fffc446079d97cd9bfdb388024
papak <bobniroomand@yahoo.com>
2016-05-11 03:06:07 +0430

    maket-e kar!

--- kooisup/src/ir/kooisup/jam/CatsNQuestions.java
+++ kooisup/src/ir/kooisup/jam/CatsNQuestions.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
 
 @ManagedBean
 @SessionScoped
@@ -13,7 +16,16 @@ public class CatsNQuestions {
 		categories.add("ورزشی");
 		categories.add("فیلم");
 		categories.add("کتاب");
-		selectedCategory = "همینطوری";
+
+
+		questions = new ArrayList<Question>();
+		ArrayList<String> choices = new ArrayList<String>();
+		choices.add("6");
+		choices.add("5");
+		choices.add("4");
+		choices.add("3");
+		Question q = new Question(1, "chanta?", "ورزشی", choices.get(0), choices);
+		questions.add(q);
 	}
 	
 	public void fetchCategories(){
@@ -26,13 +38,30 @@ public class CatsNQuestions {
 		//questions = new ArrayList<Question>(db.fetchQuestions(selectedCategory));
 	}
 	
+	public void processAction(ActionEvent event) throws AbortProcessingException {
+		//access userData bean directly
+		CatsNQuestions catsNQuestions = (CatsNQuestions) FacesContext.getCurrentInstance().
+				getExternalContext().getSessionMap().get("catsNQuestions");
+		catsNQuestions.setSelectedCategory((String)event.getComponent().getAttributes().get("catName"));
+	}
+	
+	public String addQuestion(){
+//		DBHandler db = DBHandler.getInstance();
+//		db.insertQuestion(text, selectedCategory, choices.get(0), choices);
+		ArrayList<String> choices = new ArrayList<String>();
+		choices.add(firstChoice);
+		choices.add(secChoice);
+		choices.add(thirdChoice);
+		choices.add(fourthChoice);
+		Question q = new Question(2, "chanta?", "ورزشی", choices.get(0), choices);
+		questions.add(q);
+		return "edit_questions";
+	}
+	
 	private String selectedCategory;
 	private ArrayList<Question> questions;
 	private ArrayList<String> categories;
-	
-	public ArrayList<String> getCategories() {
-		return categories;
-	}
+
 
 	public String getSelectedCategory() {
 		return selectedCategory;
@@ -40,5 +69,69 @@ public class CatsNQuestions {
 
 	public void setSelectedCategory(String selectedCategory) {
 		this.selectedCategory = selectedCategory;
+//		DBHandler db = DBHandler.getInstance();
+//		questions = new ArrayList<Question>(db.findQuestions(selectedCategory));
 	}
+
+	public ArrayList<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(ArrayList<Question> questions) {
+		this.questions = questions;
+	}
+	
+	public ArrayList<String> getCategories() {
+//		DBHandler db = DBHandler.getInstance();
+//		categories = new ArrayList<String>(db.findCategories());
+		return categories;
+	}
+	
+	private String text;
+	private String firstChoice;
+	private String secChoice;
+	private String thirdChoice;
+	private String fourthChoice;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getFirstChoice() {
+		return firstChoice;
+	}
+
+	public void setFirstChoice(String firstChoice) {
+		this.firstChoice = firstChoice;
+	}
+
+	public String getSecChoice() {
+		return secChoice;
+	}
+
+	public void setSecChoice(String secChoice) {
+		this.secChoice = secChoice;
+	}
+
+	public String getThirdChoice() {
+		return thirdChoice;
+	}
+
+	public void setThirdChoice(String thirdChoice) {
+		this.thirdChoice = thirdChoice;
+	}
+
+	public String getFourthChoice() {
+		return fourthChoice;
+	}
+
+	public void setFourthChoice(String fourthChoice) {
+		this.fourthChoice = fourthChoice;
+	}
+
+
 }
--- kooisup/src/ir/kooisup/jam/Question.java
+++ kooisup/src/ir/kooisup/jam/Question.java
@@ -3,12 +3,23 @@ package ir.kooisup.jam;
 import java.util.ArrayList;
 
 public class Question {
-	public Question(String text, ArrayList<String> choices){
+	/*public Question(String text, ArrayList<String> choices){
 		this.text = text;
 		this.choices = new ArrayList<String>(choices);
-	}
+	}*/
 	
+	public Question(int qsID, String text, String category, String answer, ArrayList<String> choices) {
+		this.qsID=qsID;
+		this.text=text;
+		this.category=category;
+		this.choices=choices;
+		this.answer=answer;
+	}
+
 	private String text;
+	private String answer;
+	private String category;
+	private int qsID;
 	ArrayList<String> choices;
 	
 	public String getText() {
@@ -18,10 +29,35 @@ public class Question {
 		this.text = text;
 	}
 	
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public int getQsID() {
+		return qsID;
+	}
+	public void setQsID(int qsID) {
+		this.qsID = qsID;
+	}
 	public ArrayList<String> getChoices() {
 		return choices;
 	}
 	public void setChoices(ArrayList<String> choices) {
 		this.choices = choices;
 	}
+	@Override
+	public String toString() {
+		return "Question [text=" + text + ", answer=" + answer + ", category=" + category + ", qsID=" + qsID
+				+ ", choices=" + choices + "]";
+	}
+	
+	
 }
