f0e102fcdc8b87d0a6cbfb14beeff872668c94de
aidamsv <moosavifar1993@yahoo.com>
2016-05-21 15:03:35 +0430

    findUser db

--- kooisup/src/ir/kooisup/jam/User.java
+++ kooisup/src/ir/kooisup/jam/User.java
@@ -1,61 +1,62 @@
 package ir.kooisup.jam;
-
 public class User {
+  
+  public User(String username, String password, String email, String gender, String country, String uuid){
+    this.username=username;
+    this.password = password;
+    this.email = email;
+    this.gender = gender;
+    this.country = country;
+    this.confirmationCode = uuid;
+    this.mailConfirmed=false;
+  }
+  
+  public User(String username, String password, String email, String gender, String country, String uuid, boolean mailConfirmed){
+	    this.username=username;
+	    this.password = password;
+	    this.email = email;
+	    this.gender = gender;
+	    this.country = country;
+	    this.confirmationCode = uuid;
+	    this.mailConfirmed=mailConfirmed;
+  }
+  
+  private String username;
+  private String password;
+  private String email;
+  private String gender;
+  private String country;
+  private String confirmationCode;
+  private boolean mailConfirmed;
+  
 
-	public User(String username, String password, String email, String gender, String country, String uuid) {
-		this.username = username;
-		this.password = password;
-		this.email = email;
-		this.gender = gender;
-		this.country = country;
-		this.confirmationCode = uuid;
-		this.mailConfirmed = false;
-	}
-
-	private String username;
-	private String password;
-	private String email;
-	private String gender;
-	private String country;
-	private String confirmationCode;
-	private boolean mailConfirmed;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public String getConfirmationCode() {
-		return confirmationCode;
-	}
-
-	public boolean isMailConfirmed() {
-		return mailConfirmed;
-	}
-
-	@Override
-	public String toString() {
-		return "User [username=" + username + ", password=" + password + ", email=" + email + ", gender=" + gender
-				+ ", country=" + country + ", confirmationCode=" + confirmationCode + ", mailConfirmed=" + mailConfirmed
-				+ "]";
-	}
-
-	public void antiBabak() {
-		System.out.println("az hamin tiribun elam mikonam ridi :|");
-	}
+  public String getUsername() {
+    return username;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public String getGender() {
+    return gender;
+  }
+  public String getCountry() {
+    return country;
+  }
+  public String getConfirmationCode() {
+    return confirmationCode;
+  }
+  public boolean isMailConfirmed() {
+    return mailConfirmed;
+  }
+@Override
+public String toString() {
+	return "User [username=" + username + ", password=" + password + ", email=" + email + ", gender=" + gender
+			+ ", country=" + country + ", confirmationCode=" + confirmationCode + ", mailConfirmed=" + mailConfirmed
+			+ "]";
+}
+  
+  
 }
